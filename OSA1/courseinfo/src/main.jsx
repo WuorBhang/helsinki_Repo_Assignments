1d6eea897d0f1f39440c0cb8171b44cc1d5122f0
WuorBhang <uhuribhang211@gmail.com>
2026-02-17 16:54:29 +0300

    adding the course info and descri

---
+++ OSA1/courseinfo/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import App1 from './App1.jsx'
+import App2 from './App2.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <div>
+        <App1 />
+        <hr />
+        <App2 />
+    </div>
+)
