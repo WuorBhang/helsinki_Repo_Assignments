67b8aa14671fcf04ca17bb67f6f63d4e6dc52642
WuorBhang <uhuribhang211@gmail.com>
2026-03-03 15:45:18 +0300

    updates the structure of the files and folders

---
+++ OSA2/courseinfo/task2.5/src/Components/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ parts }) => {
+  return (
+    <h4>Total of {parts.reduce((s, p) => s + p.exercises, 0)} exercises</h4>
+  );
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
