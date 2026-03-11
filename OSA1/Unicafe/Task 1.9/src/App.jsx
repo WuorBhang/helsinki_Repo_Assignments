f5941aaf1aa5f379ec05f21211ba8ad65bf9e07d
WuorBhang <uhuribhang211@gmail.com>
2026-03-11 16:11:17 +0300

    updates on the UNICAFE tasks fromm 1.6 - 1.14

---
+++ OSA1/Unicafe/Task 1.9/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  const average = (good - bad) / total;
+  const positivePercent = (good / total) * 100;
+
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercent} %</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
